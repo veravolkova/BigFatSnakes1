8fc8de524af8c21d255de57eb9cd5755408e47dc
Andrei Rikkiev <39307003+andreirik@users.noreply.github.com>
2018-06-05 18:38:31 +0300

    sending

--- game_file.py
+++ game_file.py
@@ -1,2 +1,9 @@
-#write your code here
-#create board
+def board(answer):
+    print(answer[7]+'|'answer[8]+'|'answer[9])
+    print('--------')
+    print(answer[4] + '|'answer[5] + '|'answer[6])
+    print('--------')
+    print(answer[1] + '|'answer[2] + '|'answer[3])
+
+answer_list=[,x,x,x,x,x,x,x,x,x]
+    print
