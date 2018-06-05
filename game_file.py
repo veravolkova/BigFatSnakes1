63b8bfd52295fca612bf14a2d0308ed41ca82d3d
veravolkova <verap87@mail.ru>
2018-06-05 16:35:46 +0100

    here is veras board

--- game_file.py
+++ game_file.py
@@ -1,2 +1,14 @@
-#write your code here
-#create board
+def display_board(board):      
+
+    
+    print(' - - - - - - - - - -')   
+    print(' |  ' + board[1] +'  |  ' + board[2] + '  |  ' + board[3] + '  |')
+    print(' - - - - - - - - - -')  
+    print(' |  ' + board[4] +'  |  ' + board[5] + '  |  ' + board[6] + '  |')  
+    print(' - - - - - - - - - -')
+    print(' |  ' + board[7] +'  |  ' + board[8] + '  |  ' + board[9] + '  |')   
+    print(' - - - - - - - - - -')
+
+#test
+#test_board = ['#','X','O','X','O','X','O','X','O','X']
+#display_board(test_board)
