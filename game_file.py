069632751b08736f3787765b0416a1f49a3285ee
Andrei Rikkiev <39307003+andreirik@users.noreply.github.com>
2018-06-08 16:28:29 +0300

    Andrei Part 6,7

--- game_file.py
+++ game_file.py
@@ -1,9 +1,17 @@
-def board(answer):
-    print(answer[7]+'|'answer[8]+'|'answer[9])
-    print('--------')
-    print(answer[4] + '|'answer[5] + '|'answer[6])
-    print('--------')
-    print(answer[1] + '|'answer[2] + '|'answer[3])
+## Step 6: Write a function that returns a boolean indicating whether a space on the board is freely available.
 
-answer_list=[,x,x,x,x,x,x,x,x,x]
-    print
+def space_check(board, position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+##Step 7: Write a function that checks if the board is full and returns a boolean value. True if full, False otherwise.
+
+def player_choice(board):
+    space = 0
+    for cell in range(1,10):
+        if space_check(board, cell):
+            return False
+        else:
+            return True
