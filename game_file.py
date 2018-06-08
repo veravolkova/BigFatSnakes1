0a97b639833e27da93545c5773be1d30032b09a6
Tiina Antikainen <tiina@meapu.com>
2018-06-08 22:32:42 +0300

    Update game_file.py

--- game_file.py
+++ game_file.py
@@ -1,4 +1,4 @@
-# I do not know why this code does not work. 
+# I do not know why this code does not work. Does someone have any ideas?
 #It prints out the input question, but even user input is X the loop continues running. This in Jupyter.
 #Step 2:
 def player_input():
@@ -8,3 +8,12 @@ def player_input():
         a = input("What are you:")
     if a == "O"or a== "X":
         print("You have selected:" + a)
+        
+        
+#Step 3:
+#I could not make this work either. I am not sure how to define the board that is made by the display_board function. Otherwise I think 
+#marker needs to be same as the position. After the marker is in right position updated display board would be printed out.
+
+def place_marker(board,marker,position):
+    board[position] = marker
+    display_board()
