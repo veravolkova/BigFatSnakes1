38e76b81b68fe72136ad3fe214f744a680e96949
jolene hoang <39855605+jolenehoangtu@users.noreply.github.com>
2018-06-10 12:49:47 +0300

    Update game_file.py

--- game_file.py
+++ game_file.py
@@ -1,2 +1,23 @@
-#write your code here
-#create board
+#section8
+def player_choice(board):
+
+    #Use While Loop: the next position is repeatedly tested until the condition is met. 
+   
+    position = 0
+    
+    while position not space_check(board, position):
+        position = int(input('Your next position is (from 1-9): '))
+        
+    return position
+
+    else: 
+        return ('please choose another number')
+  
+    #return instead of print because it is invisible in the output. 
+    
+    pass
+  
+ #section9
+ def replay():
+    return input('Try again? Y or N: ').startswith('y')
+    pass
